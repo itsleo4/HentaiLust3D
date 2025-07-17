21ec0e18c79f1f2e4a2f7a16b3a98e9736402287
itsleo4 <bholanitin308@gmail.com>
2025-07-17 22:25:14 +0530

    Added success page after video upload

--- app,.py
+++ app,.py
@@ -1,19 +1,31 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        category = request.form['category']
+        hashtags = request.form['hashtags']
+        
+        video = request.files['video']
+        thumbnail = request.files['thumbnail']
 
-@app.route('/pro')
-def pro():
-    return render_template('pro.html')
+        video_path = os.path.join(UPLOAD_FOLDER, video.filename)
+        thumbnail_path = os.path.join(UPLOAD_FOLDER, thumbnail.filename)
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
+        video.save(video_path)
+        thumbnail.save(thumbnail_path)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        return render_template('success.html',
+                               title=title,
+                               description=description,
+                               video_url='/' + video_path,
+                               thumbnail_url='/' + thumbnail_path)
+
+    return render_template('upload.html')
